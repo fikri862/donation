8990a77003af8324e50b3afa0b720e677089298b
fikri862 <fikrihaykalnasution@gmail.com>
2024-01-04 21:28:22 +0700

    create migration & model

---
+++ app/Models/Donation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function setStatusPanding () {
+        $this->attributes['status'] ='panding';
+        self::save();
+    }
+
+    public function setStatusSuccess () {
+        $this->attributes['status'] ='success';
+        self::save();
+    }
+
+    public function setStatusFailed () {
+        $this->attributes['status'] ='failed';
+        self::save();
+    }
+
+    public function setStatusExpired () {
+        $this->attributes['status'] ='expired';
+        self::save();
+    }
+}
